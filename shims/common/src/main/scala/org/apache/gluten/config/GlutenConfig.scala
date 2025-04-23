f91a423c7a38702ce72e30ed88178f9a1e7d03e0
Rong Ma <marong@apache.org>
2025-04-23 21:46:19 +0100

    [GLUTEN-9163][VL] Separate compression buffer and disk write buffer configuration (#9356)

--- shims/common/src/main/scala/org/apache/gluten/config/GlutenConfig.scala
+++ shims/common/src/main/scala/org/apache/gluten/config/GlutenConfig.scala
@@ -203,6 +203,9 @@ class GlutenConfig(conf: SQLConf) extends Logging {
   def columnarShuffleReaderBufferSize: Long =
     getConf(COLUMNAR_SHUFFLE_READER_BUFFER_SIZE)
 
+  def columnarSortShuffleDeserializerBufferSize: Long =
+    getConf(COLUMNAR_SORT_SHUFFLE_DESERIALIZER_BUFFER_SIZE)
+
   def maxBatchSize: Int = getConf(COLUMNAR_MAX_BATCH_SIZE)
 
   def shuffleWriterBufferSize: Int = getConf(SHUFFLE_WRITER_BUFFER_SIZE)
@@ -1149,6 +1152,14 @@ object GlutenConfig {
       .doc("Buffer size in bytes for shuffle reader reading input stream from local or remote.")
       .bytesConf(ByteUnit.BYTE)
       .createWithDefaultString("0")
+
+  val COLUMNAR_SORT_SHUFFLE_DESERIALIZER_BUFFER_SIZE =
+    buildConf("spark.gluten.sql.columnar.shuffle.sort.deserializerBufferSize")
+      .internal()
+      .doc("Buffer size in bytes for sort-based shuffle reader deserializing raw input to " +
+        "columnar batch.")
+      .bytesConf(ByteUnit.BYTE)
+      .createWithDefaultString("1MB")
 
   val COLUMNAR_MAX_BATCH_SIZE =
     buildConf("spark.gluten.sql.columnar.maxBatchSize")

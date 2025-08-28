apache/incubator-gluten

[Core] Support Jvm memory shrinking for DynamicOffHeapSizingMemoryTarget (#9585)
[Core] Support Jvm memory shrinking for DynamicOffHeapSizingMemoryTarget

gluten-core/src/main/java/org/apache/gluten/memory/memtarget/spark/TreeMemoryConsumers.java
@@ -21,6 +21,7 @@ import org.apache.gluten.memory.memtarget.Spillers;
 import org.apache.gluten.memory.memtarget.TreeMemoryTarget;
 
 import org.apache.commons.collections.map.ReferenceMap;
+import org.apache.spark.memory.MemoryMode;
 import org.apache.spark.memory.TaskMemoryManager;
 import org.apache.spark.util.Utils;
 
@@ -45,7 +46,11 @@ public final class TreeMemoryConsumers {
     private final Map<Long, TreeMemoryTarget> roots = new ConcurrentHashMap<>();
 
     private Factory(TaskMemoryManager tmm) {
-      this.sparkConsumer = new TreeMemoryConsumer(tmm);
+      MemoryMode mode =
+          GlutenCoreConfig.get().dynamicOffHeapSizingEnabled()
+              ? MemoryMode.ON_HEAP
+              : MemoryMode.OFF_HEAP;
+      this.sparkConsumer = new TreeMemoryConsumer(tmm, mode);
     }
 
     private TreeMemoryTarget ofCapacity(long capacity) {

gluten-core/src/main/scala/org/apache/spark/memory/GlobalOffHeapMemoryTarget.scala
@@ -16,6 +16,7 @@
  */
 package org.apache.spark.memory
 
+import org.apache.gluten.config.GlutenCoreConfig
 import org.apache.gluten.exception.GlutenException
 import org.apache.gluten.memory.{MemoryUsageRecorder, SimpleMemoryUsageRecorder}
 import org.apache.gluten.memory.memtarget.{KnownNameAndStats, MemoryTarget, MemoryTargetUtil, MemoryTargetVisitor}
@@ -34,6 +35,9 @@ class GlobalOffHeapMemoryTarget private[memory]
   with Logging {
   private val targetName = MemoryTargetUtil.toUniqueName("GlobalOffHeap")
   private val recorder: MemoryUsageRecorder = new SimpleMemoryUsageRecorder()
+  private val mode: MemoryMode =
+    if (GlutenCoreConfig.get.dynamicOffHeapSizingEnabled) MemoryMode.ON_HEAP
+    else MemoryMode.OFF_HEAP
 
   private val FIELD_MEMORY_MANAGER: Field = {
     val f =
@@ -54,10 +58,7 @@ class GlobalOffHeapMemoryTarget private[memory]
       .map {
         mm =>
           val succeeded =
-            mm.acquireStorageMemory(
-              BlockId(s"test_${UUID.randomUUID()}"),
-              size,
-              MemoryMode.OFF_HEAP)
+            mm.acquireStorageMemory(BlockId(s"test_${UUID.randomUUID()}"), size, mode)
 
           if (succeeded) {
             recorder.inc(size)
@@ -65,6 +66,17 @@ class GlobalOffHeapMemoryTarget private[memory]
           } else {
             // OOM.
             // Throw OOM.
+            if (mode == MemoryMode.ON_HEAP) {
+              val storageUsed = mm.onHeapStorageMemoryUsed
+              val executionUsed = mm.onHeapExecutionMemoryUsed
+              val onHeapMemoryTotal = storageUsed + executionUsed
+              logError(
+                s"Spark on-heap memory is exhausted. " +
+                  s"Requested: $size, " +
+                  s"Storage: $storageUsed / $onHeapMemoryTotal, " +
+                  s"Execution: $executionUsed / $onHeapMemoryTotal")
+              return 0;
+            }
             val storageUsed = mm.offHeapStorageMemoryUsed
             val executionUsed = mm.offHeapExecutionMemoryUsed
             val offHeapMemoryTotal = storageUsed + executionUsed
@@ -82,7 +94,7 @@ class GlobalOffHeapMemoryTarget private[memory]
     memoryManagerOption()
       .map {
         mm =>
-          mm.releaseStorageMemory(size, MemoryMode.OFF_HEAP)
+          mm.releaseStorageMemory(size, mode)
           recorder.inc(-size)
           size
       }

warrenzhu25/gluten

(cherry picked from commit 07b18e75db083630b9c1f141f11a4a7894a404f7)
# Conflicts:
#	gluten-core/src/main/java/org/apache/gluten/memory/memtarget/DynamicOffHeapSizingMemoryTarget.java
#	gluten-core/src/main/scala/org/apache/gluten/GlutenPlugin.scala
#	gluten-core/src/main/scala/org/apache/gluten/config/GlutenCoreConfig.scala
Change-Id: Ic439c4a89abf1dcbdb82c0eae2d66f9b5f93eb42
Reviewed-on: https://bigdataoss-internal-review.googlesource.com/c/third_party/apache/incubator-gluten/+/115780
Reviewed-by: Revanth Venkat Mikkilineni <revanthvenkat@google.com>
Reviewed-by: Adesh Rao <adeshr@google.com>
Tested-by: Srinivas S T <srst@google.com>

gluten-core/src/main/java/org/apache/gluten/memory/memtarget/spark/TreeMemoryConsumers.java
@@ -21,6 +21,7 @@ import org.apache.gluten.memory.memtarget.Spillers;
 import org.apache.gluten.memory.memtarget.TreeMemoryTarget;
 
 import org.apache.commons.collections.map.ReferenceMap;
+import org.apache.spark.memory.MemoryMode;
 import org.apache.spark.memory.TaskMemoryManager;
 import org.apache.spark.util.Utils;
 
@@ -45,7 +46,11 @@ public final class TreeMemoryConsumers {
     private final Map<Long, TreeMemoryTarget> roots = new ConcurrentHashMap<>();
 
     private Factory(TaskMemoryManager tmm) {
-      this.sparkConsumer = new TreeMemoryConsumer(tmm);
+      MemoryMode mode =
+          GlutenConfig.get().dynamicOffHeapSizingEnabled()
+              ? MemoryMode.ON_HEAP
+              : MemoryMode.OFF_HEAP;
+      this.sparkConsumer = new TreeMemoryConsumer(tmm, mode);
     }
 
     private TreeMemoryTarget ofCapacity(long capacity) {

gluten-core/src/main/scala/org/apache/spark/memory/GlobalOffHeapMemoryTarget.scala
@@ -16,6 +16,7 @@
  */
 package org.apache.spark.memory
 
+import org.apache.gluten.config.GlutenConfig
 import org.apache.gluten.exception.GlutenException
 import org.apache.gluten.memory.{MemoryUsageRecorder, SimpleMemoryUsageRecorder}
 import org.apache.gluten.memory.memtarget.{KnownNameAndStats, MemoryTarget, MemoryTargetUtil, MemoryTargetVisitor}
@@ -34,6 +35,9 @@ class GlobalOffHeapMemoryTarget private[memory]
   with Logging {
   private val targetName = MemoryTargetUtil.toUniqueName("GlobalOffHeap")
   private val recorder: MemoryUsageRecorder = new SimpleMemoryUsageRecorder()
+  private val mode: MemoryMode =
+    if (GlutenConfig.get.dynamicOffHeapSizingEnabled) MemoryMode.ON_HEAP
+    else MemoryMode.OFF_HEAP
 
   private val FIELD_MEMORY_MANAGER: Field = {
     val f =
@@ -54,10 +58,7 @@ class GlobalOffHeapMemoryTarget private[memory]
       .map {
         mm =>
           val succeeded =
-            mm.acquireStorageMemory(
-              BlockId(s"test_${UUID.randomUUID()}"),
-              size,
-              MemoryMode.OFF_HEAP)
+            mm.acquireStorageMemory(BlockId(s"test_${UUID.randomUUID()}"), size, mode)
 
           if (succeeded) {
             recorder.inc(size)
@@ -65,6 +66,17 @@ class GlobalOffHeapMemoryTarget private[memory]
           } else {
             // OOM.
             // Throw OOM.
+            if (mode == MemoryMode.ON_HEAP) {
+              val storageUsed = mm.onHeapStorageMemoryUsed
+              val executionUsed = mm.onHeapExecutionMemoryUsed
+              val onHeapMemoryTotal = storageUsed + executionUsed
+              logError(
+                s"Spark on-heap memory is exhausted. " +
+                  s"Requested: $size, " +
+                  s"Storage: $storageUsed / $onHeapMemoryTotal, " +
+                  s"Execution: $executionUsed / $onHeapMemoryTotal")
+              return 0;
+            }
             val storageUsed = mm.offHeapStorageMemoryUsed
             val executionUsed = mm.offHeapExecutionMemoryUsed
             val offHeapMemoryTotal = storageUsed + executionUsed
@@ -82,7 +94,7 @@ class GlobalOffHeapMemoryTarget private[memory]
     memoryManagerOption()
       .map {
         mm =>
-          mm.releaseStorageMemory(size, MemoryMode.OFF_HEAP)
+          mm.releaseStorageMemory(size, mode)
           recorder.inc(-size)
           size
       }